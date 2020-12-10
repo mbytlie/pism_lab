601026e365f3f730afdb2e6eb1419306b11240e8
myosotis drakonis <maya.itilven@gmail.com>
2020-12-10 05:53:06 +0300

    change by adding

--- lab8/src/Main.java
+++ lab8/src/Main.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("stray");
+        list.add("kids");
+        list.add("everywhere");
+        list.add("all");
+        list.add("around");
+        list.add("the");
+        list.add("world");
+        list.add("you");
+        list.add("make");
+        list.add("stray");
+        list.add("kids");
+        list.add("stay");
+        list.add("stream");
+        list.add("back");
+        list.add("door");
+        list.add("for");
+        list.add("good");
+        list.add("health");
+
+        ArrayList<Integer> length = new ArrayList<>();
+        for(String word: list){
+            length.add(word.length());
+        }
+
+        Collections.sort(length);
+        System.out.println(length);
+    }
 }
+
